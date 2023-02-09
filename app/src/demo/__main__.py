a012a608771efce9c8c3205028c53d81918ce958
vviilloovv <43818483+vviilloovv@users.noreply.github.com>
2023-02-09 17:20:27 +0900

    DB読み書き処理追加

--- app/src/demo/__main__.py
+++ app/src/demo/__main__.py
@@ -1,8 +1,43 @@
 from demo.fib import fib
+from demo.db.setting import session
+from demo.db.models.user import User
+from demo.db.models.book import Book
+
+
+def add_user(name: str, age: int):
+    user = User()
+    user.name = name
+    user.age = age
+    session.add(user)
+    session.commit()
+
+
+def all_users() -> list[User]:
+    return session.query(User).all()
+
+
+def add_book(title: str, author: str):
+    book = Book()
+    book.title = title
+    book.author = author
+    session.add(book)
+    session.commit()
+
+
+def all_books() -> list[Book]:
+    return session.query(Book).all()
 
 
 def main():
     print(fib(5))
+    add_user(name="Alice", age=17)
+    add_book(title="SLAM DUNK", author="井上雄彦")
+
+    for user in all_users():
+        print(f"id: {user.id} name: {user.name} age: {user.age}")
+
+    for book in all_books():
+        print(f"id: {book.id} title: {book.title} author: {book.author}")
 
 
 if __name__ == "__main__":
